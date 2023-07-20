854b162d2af295c9a35e46035a31526101c38e2c
Andres Gaibor <androymartin99@gmail.com>
2023-07-20 05:12:38 -0500

    algunos estilos

--- src/ProductCard.tsx
+++ src/ProductCard.tsx
@@ -9,16 +9,26 @@ export interface Product {
   link?: string;
 }
 
+function truncateText(text = "", maxLength: number) {
+  if (text.length <= maxLength) {
+    return text;
+  } else {
+    return text.slice(0, maxLength) + "...";
+  }
+}
+
 const ProductCard: React.FC<{ product: Product }> = ({ product }) => {
   const { title, price, image, link } = product;
 
   return (
     <div className="max-w-xs mx-auto mb-4 w-full">
       <div className="flex">
-        <img src={image} alt={title} className="w-24 h-24 object-cover" />
+        <img src={image} alt={title} className="w-44 h-44 object-cover" />
         <div className="ml-4">
-          <h2 className="text-lg font-semibold mb-2">{title}</h2>
-          <p className="text-base font-bold mb-2">{price}</p>
+          <h2 className="text-base font-semibold mb-2">
+            {truncateText(title, 75)}
+          </h2>
+          <p className="font-bold mb-2 text-xl">$ {price}</p>
           <a
             href={link}
             target="_blank"
